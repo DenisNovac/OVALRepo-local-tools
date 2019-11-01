1e737f98f2ca9131ca599d1299a440c3b02aa03c
DenisNovac <ponydnk@gmail.com>
2019-11-01 18:29:25 +0500

    Trying to optimize indexing

--- modules/build_definition.py
+++ modules/build_definition.py
@@ -1,5 +1,6 @@
-import os, shutil, sys
+import os, shutil, sys, random
 from git import Repo
+import git.exc
 sys.path.insert(1, os.path.relpath('./ScriptsEnvironment/scripts'))
 import ScriptsEnvironment.scripts.build_oval_definitions_file as build_oval_definitions_file
 
@@ -26,22 +27,29 @@ def build_definition(args):
   
     # creating fake environment for scripts
     try:
-        shutil.rmtree(os.path.relpath('./ScriptsEnvironment/.git'))
+        #shutil.rmtree(os.path.relpath('./ScriptsEnvironment/.git'))
         os.remove(os.path.relpath('./ScriptsEnvironment/.init'))
     except:
         pass
 
-    open(os.path.relpath('./ScriptsEnvironment/.init'), 'w').close()
-    repo = Repo.init(os.path.relpath('./ScriptsEnvironment'))
+    with open(os.path.relpath('./ScriptsEnvironment/.init'), 'w') as file:
+        for i in range(0,3):
+            r=int(random.random()*100)
+            file.write(str(i))
+    try:
+        repo = Repo(os.path.relpath('./ScriptsEnvironment'))
+    except git.exc.InvalidGitRepositoryError:
+        repo = Repo.init(os.path.relpath('./ScriptsEnvironment'))
+    
     index = repo.index
-    index.add( [ '.init' ] )
-    index.commit("init")
+    index.add( [ '*' ] )
+    index.commit("env")
 
     build_oval_definitions_file.main()
 
     # removing fake environment for next use
     try:
-        shutil.rmtree(os.path.relpath('./ScriptsEnvironment/.git'))
+        #shutil.rmtree(os.path.relpath('./ScriptsEnvironment/.git'))
         os.remove(os.path.relpath('./ScriptsEnvironment/.init'))
     except:
         pass
